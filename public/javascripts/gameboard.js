eeec009029593419d63c08de4399b4fe33ec6c7e
veenamadhu17 <63911638+veenamadhu17@users.noreply.github.com>
2021-01-21 10:01:48 +0400

    Update gameboard.js

--- public/javascripts/gameboard.js
+++ public/javascripts/gameboard.js
@@ -104,7 +104,7 @@ function GameState(hexGrid, sb){
  * timecount - used to start the timecount
  * 
  */
-socket = new WebSocket("ws://localhost:3000");
+socket = new WebSocket("https://hexifybyvandb.herokuapp.com/");
 
 let hexGrid = new Map();
 let movesMade = [];
